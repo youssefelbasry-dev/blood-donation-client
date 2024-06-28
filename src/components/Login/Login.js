789a5b68fb0eea58d6f2c99f1c94584d47fb6aef
Youssef <youssefelbasry85@gmail.com>
2024-06-28 01:05:10 +0100

    last dance deployement

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,75 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Login.css'
+import  Sheild from '../../pictures/lr122.png'
+import  logo from '../../pictures/docteur.png'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserShield, faKey, faArrowRight, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from "../redux/apiCalls";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [passwordType, setPasswordType] = useState('password');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const { isFetching} = useSelector((state) => state.user);
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-  const { isFetching} = useSelector((state) => state.user);
-    
-  const handleClick = (e) => {
-    e.preventDefault();
-    login(dispatch, { username, password });
-  };
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(dispatch, { username, password });
+    };
 
+    const togglePasswordVisibility = (e) => {
+        e.preventDefault();
+        setPasswordType(passwordType === 'password' ? 'text' : 'password');
+    }
   return (
-    <div className='logbg'>
-    <div class="login">
-        <h1>Login</h1><br/>
-        <form>
-            <input type="text" name="username" placeholder="Username" required="required" className='inp' onChange={(e) => setUsername(e.target.value)} autoComplete="username"  />
-            <input type="password" name="password" placeholder="Password" required="required" className='inp' onChange={(e) => setPassword(e.target.value)} autoComplete="current-password"  />
-            <button type="submit" onClick={handleClick} disabled={isFetching}>Let me in</button>
-        </form>
-    </div>
-    <Link to='/register' className='notalredy'>Create An Account!</Link>
+    <div className='Login'>
+        <div className='LoginDS'>
+            <div className='LoginImg'>
+                <div className='imgdivsheild'>
+                    <img src={Sheild} alt='sheild'/>
+                </div>
+                <h1>BloodLink</h1>
+                <p>Plateforme en ligne facilitant l'inscription des donneurs de sang,<br/> la recherche de donneurs par groupe sanguin et localisation..</p>
+            </div>
+        </div>
+        <div className='LoginDiv'>
+            <div className='LoginInfo'>
+                <div className='imglg'>
+                     <img src={logo} alt='logo'/>
+                </div>
+                <h1>BloodLink!</h1>
+                <p>Bienvenue sur notre page de connexion !<br/>
+                Veuillez saisir vos identifiants ci-dessous.</p>
+            </div>
+            <div className='LoginForm'>
+            <form className="formulaire">
+                <div className="textbox">
+                    <input type="text" className='inputLg' onChange={(e) => setUsername(e.target.value)} autoComplete="username" placeholder='Entre Username...'/>
+                    <label>Username</label>
+                    <FontAwesomeIcon icon={faUserShield} className='usr'/>
+                </div>
+                <div className="textbox">
+                    <input type={passwordType} className='inputLg' onChange={(e) => setPassword(e.target.value)} autoComplete="current-password" placeholder='Entre Mot De Passe...'/>
+                    <label>Mot De Passe</label>
+                    <FontAwesomeIcon icon={faKey} className='usr'/>
+                </div>
+                <div className='btnbnr'>
+                    <button type="submit" onClick={handleClick} disabled={isFetching}>
+                        Se Connecter
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
+                    <button id='btnshowpwd' onClick={togglePasswordVisibility}>
+                        {passwordType === 'password' ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}  
+                    </button>
+                </div>
+                <Link className='noAcount' to='/register'>I don't Have An Account!</Link>
+            </form>
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,18 +3,22 @@ import './Register.css'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import  Sheild from '../../pictures/lr122.png'
+import  logo from '../../pictures/docteur.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserShield, faKey, faArrowRight, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 const Register = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [passwordType, setPasswordType] = useState('password');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("https://blood-donation-api-1.onrender.com/auth/register", {
+      const response = await axios.post("http://localhost:3500/auth/register", {
         username,
         email,
         password,
@@ -25,19 +29,60 @@ const Register = () => {
       console.error(error);
     }
   };
-
+  const togglePasswordVisibility = (e) => {
+    e.preventDefault();
+    setPasswordType(passwordType === 'password' ? 'text' : 'password');
+  }
   return (
-    <div className='regbg'>
-    <div class="register">
-        <h1>Register</h1><br/>
-        <form method="post">
-            <input type="text" name="username" placeholder="Username" required="required" className='inp' onChange={(e) => setUsername(e.target.value)} autoComplete="username"  />
-            <input type="email" name="email" placeholder="Email" required="required" className='inp' onChange={(e) => setEmail(e.target.value)} autoComplete="Email"  />
-            <input type="password" name="password" placeholder="Password" required="required" className='inp' onChange={(e) => setPassword(e.target.value)} autoComplete="current-password"  />
-            <button type="submit" class="loginbtn" id='loginbtn' onClick={handleSubmit}>Let me in</button>
-        </form>
-    </div>
-    <Link to='/Login' className='alredy'>Alredy Have Account!</Link>
+    <div className='Login'>
+        <div className='LoginDS'>
+            <div className='LoginImg'>
+                <div className='imgdivsheild'>
+                    <img src={Sheild} alt='sheild'/>
+                </div>
+                <h1>BloodLink</h1>
+                <p>Plateforme en ligne facilitant l'inscription des donneurs de sang,<br/> la recherche de donneurs par groupe sanguin et localisation..</p>
+            </div>
+        </div>
+        <div className='LoginDiv'>
+            <div className='LoginInfo'>
+                <div className='imglg'>
+                     <img src={logo} alt='logo'/>
+                </div>
+                <h1>BloodLink!</h1>
+                <p>Bienvenue sur notre page de registrement !<br/>
+                Veuillez saisir vos identifiants ci-dessous.</p>
+            </div>
+            <div className='LoginForm'>
+            <form className="formulaire">
+                <div className="textboxx">
+                    <input type="text" className='inputLg' onChange={(e) => setUsername(e.target.value)} autoComplete="username" placeholder='Entre Username...'/>
+                    <label>Username</label>
+                    <FontAwesomeIcon icon={faUserShield} className='usr'/>
+                </div>
+                <div className="textboxx">
+                    <input type="email" className='inputLg' onChange={(e) => setEmail(e.target.value)} autoComplete="email" placeholder='Entre Email...'/>
+                    <label>Email Address</label>
+                    <FontAwesomeIcon icon={faUserShield} className='usr'/>
+                </div>
+                <div className="textboxx">
+                    <input type={passwordType} className='inputLg' onChange={(e) => setPassword(e.target.value)} autoComplete="current-password" placeholder='Entre Mot De Passe...'/>
+                    <label>Mot De Passe</label>
+                    <FontAwesomeIcon icon={faKey} className='usr'/>
+                </div>
+                <div className='btnbnr'>
+                    <button type="submit" onClick={handleSubmit}>
+                        Register
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
+                    <button id='btnshowpwd' onClick={togglePasswordVisibility}>
+                        {passwordType === 'password' ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}  
+                    </button>
+                </div>
+                <Link className='noAcount' to='/login'>Already Have An Account!</Link>
+            </form>
+            </div>
+        </div>
     </div>
   )
 }
